f69b5490a0f4edb8834d7dacfffc81366d9d8bbd
DroneBuster <matas.razgunas@gmail.com>
2016-03-10 07:54:57 +0200

    RC_Channel: Add throttle aux channel

--- libraries/RC_Channel/RC_Channel_aux.h
+++ libraries/RC_Channel/RC_Channel_aux.h
@@ -74,6 +74,7 @@ public:
         k_motor6                = 38,
         k_motor7                = 39,
         k_motor8                = 40,
+        k_throttle              = 41,
         k_nr_aux_servo_functions         ///< This must be the last enum value (only add new values _before_ this one)
     } Aux_servo_function_t;
 
